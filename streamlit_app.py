40947ee4107a22f3cf3cbdb454fb08031e6a546a
80708 <142992657+80708@users.noreply.github.com>
2023-08-23 21:30:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,5 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['avacado',
 
 # Display the table on the page
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+fruits_to_show = my_fruit_list.loc[fruits_selected]
